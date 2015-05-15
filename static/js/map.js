7b5e252d5c658396f42af01ff59ee350fa751a61
Fabio Souto <fsoutomoure@gmail.com>
2015-05-15 16:24:43 +0200

    Added portada

--- static/js/map.js
+++ static/js/map.js
@@ -121,8 +121,40 @@ var pointsData = L.geoJson(null, {
   }
 });
 
+
+// Fetch the JSON data and add it to the map
 $.getJSON("/static/data/fairua.geojson", function (data) {
   pointsData.addData(data);
 });
 
 pointsData.addTo(map);
+
+
+// Handle videos
+$('.youtube-video')
+.attr('rel', 'media-gallery')
+.fancybox({
+    'beforeShow': function(){
+        $(window).on({
+            'resize.fancybox' : function(){
+                $.fancybox.update();
+            }
+        });
+     },
+     'afterClose': function(){
+          $(window).off('resize.fancybox');
+     },
+    width: "100%",
+    padding: "0",
+    margin: [15, 25, 15, 20],
+    openEffect : 'none',
+    closeEffect : 'none',
+    prevEffect : 'none',
+    nextEffect : 'none',
+
+    arrows : false,
+    helpers : {
+        media : {},
+        buttons : {}
+    }
+});
