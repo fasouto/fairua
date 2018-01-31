13f3396661f62279ed068cfcf47ebb6e511a9368
Fabio Souto <fsoutomoure@gmail.com>
2018-01-31 20:52:20 +0100

    Lazy load dont work fine

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@
 gulp.task('js', function(){
  return gulp.src([
      'node_modules/jquery/dist/jquery.js',
-     'node_modules/lazysizes/lazysizes.js',
      'node_modules/leaflet/dist/leaflet.js',
      'node_modules/leaflet-groupedlayercontrol/dist/*.js',
      'static/js/fancybox/source/jquery.fancybox.js',
